49121ab4e21840f7e831b28c4b829add434a1081
VLinkus <LinkusVyte@gmail.com>
2023-12-11 14:25:01 +0200

    M4 Collections Task1 Completed

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,14 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> createdLinkedList = new LinkedList<>();
+        for (Integer i: sourceList){
+            if(i%2 == 0){
+                createdLinkedList.addLast(i);
+            } else {
+                createdLinkedList.addFirst(i);
+            }
+        }
+        return createdLinkedList;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,31 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        ListComparator comparator = new ListComparator();
+        sourceList.sort(comparator);
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        try {
+            int first = Integer.parseInt(String.valueOf(a));
+            int second = Integer.parseInt(String.valueOf(b));
+            int newFirst = applicationFunction(first);
+            int newSecond =  applicationFunction(second);
+            if(newFirst == newSecond) {
+                return Integer.compare(first, second);
+            } else {
+                return Integer.compare(newFirst, newSecond);
+            }
+        } catch (NumberFormatException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public int applicationFunction(int x){
+        return (5 * x * x) + 3;
     }
 }
